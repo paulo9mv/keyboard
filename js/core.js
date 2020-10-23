81abd2c5df67660201669c7db8e66d36a80a1710
Paulo Viana <paulo9mv@yahoo.com.br>
2020-10-23 15:00:32 -0400

    Adiciona map de keys ABNT2

--- js/core.js
+++ js/core.js
@@ -10,9 +10,64 @@ $(document).on("keypress", function (e) {
     console.log('keypress', e.which, e.keyCode, asciiToChar(e.keyCode))
 });
 
-const keys = {
-    'a': 'b'
+const keysType = {
+    "abnt2": {
+        'key0': ["'", '"'],
+        'key1': ['1', '!', '¹'],
+        'key2': ['2', '@', '²'],
+        'key3': ['3', '#', '³'],
+        'key4': ['4', '$', '£'],
+        'key5': ['5', '%', '¢'],
+        'key6': ['6', '¨', '¬'],
+        'key7': ['7', '&'],
+        'key8': ['8', '*'],
+        'key9': ['9', '('],
+        'key10': ['0', ')'],
+        'key11': ['-', '_'],
+        'key12': ['=', '+', '§'],
+        //
+        'key13': ['q', 'Q'],
+        'key14': ['w', 'W'],
+        'key15': ['e', 'E'],
+        'key16': ['r', 'R'],
+        'key17': ['t', 'T'],
+        'key18': ['y', 'Y'],
+        'key19': ['u', 'U'],
+        'key20': ['i', 'I'],
+        'key21': ['o', 'O'],
+        'key22': ['p', 'P'],
+        'key23': ['´', '`'],
+        'key24': ['[', '{', 'ª'],
+        //
+        'key25': ['a', 'A'],
+        'key26': ['s', 'S'],
+        'key27': ['d', 'D'],
+        'key28': ['f', 'F'],
+        'key29': ['g', 'G'],
+        'key30': ['h', 'H'],
+        'key31': ['j', 'J'],
+        'key32': ['k', 'K'],
+        'key33': ['l', 'L'],
+        'key34': ['ç', 'Ç'],
+        'key35': ['~', '^'],
+        'key36': [']', '}', 'º'],
+        //
+        'key37': ['\\', '|'],
+        'key38': ['z', 'Z'],
+        'key39': ['x', 'X'],
+        'key40': ['c', 'C'],
+        'key41': ['v', 'V'],
+        'key42': ['b', 'B'],
+        'key43': ['n', 'N'],
+        'key44': ['m', 'M'],
+        'key45': [',', '<'],
+        'key46': ['.', '>'],
+        'key47': [';', ':'],
+        'key48': ['/', '?', '°'],
+    }
 }
+
+console.log(keysType['ª'])
 
 $(document).on("keyup", function (e) {
     const index = pressing.indexOf(e.which);
@@ -25,9 +80,9 @@ $(document).on("keyup", function (e) {
 function setBackgroundColor(key, status, remove) {
     const doc = elementById(asciiToChar(key))
 
-    if(!doc) return
+    if (!doc) return
 
-    if(remove) {
+    if (remove) {
         doc.classList.remove(remove)
     }
 
@@ -45,7 +100,7 @@ function elementById(id) {
 function resetKeyboard() {
     const keyDivs = document.getElementsByClassName('keycontainer')
 
-    for(const element of keyDivs) {
+    for (const element of keyDivs) {
         element.classList.remove('pressed')
     }
 }
